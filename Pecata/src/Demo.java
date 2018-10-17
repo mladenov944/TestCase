a01466a1e253486b558eebd7289ef553992eee2d
Ivelin <Ivelin@DESKTOP-S3N1FPT>
2018-10-17 16:13:15 +0300

    first commit

---
+++ Pecata/src/Demo.java
@@ -0,0 +1,14 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		Person p = new Person((byte) 15);
+
+		System.out.println(p.getAge() + " godniki");
+		System.out.println(p.getName() + " imence");
+		System.out.println("new");
+
+	}
+
+}
---
+++ Pecata/src/Person.java
@@ -0,0 +1,27 @@
+
+public class Person {
+
+	private String name;
+	private byte age;
+
+	public Person(byte age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public byte getAge() {
+		return age;
+	}
+
+	public void setAge(byte age) {
+		this.age = age;
+	}
+
+}
